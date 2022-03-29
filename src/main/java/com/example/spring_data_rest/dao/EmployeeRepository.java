26ce83543808112f7b8963d52073279ac09149c7
Oleg Sergienko <keep233@gmail.com>
2022-03-29 23:42:24 +0300

    + dependency: "spring-boot-starter-actuator" +annotation Repository

--- src/main/java/com/example/spring_data_rest/dao/EmployeeRepository.java
+++ src/main/java/com/example/spring_data_rest/dao/EmployeeRepository.java
@@ -3,6 +3,8 @@ package com.example.spring_data_rest.dao;
 
 import com.example.spring_data_rest.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
